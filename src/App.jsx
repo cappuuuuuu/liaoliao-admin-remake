f134ea2a9844c93f330bfa4f08d658d265d85fd5
cappuuuuuu <w1814946281@gmail.com>
2023-01-01 02:47:12 +0800

    feat: add reset & global style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { GlobalStyle } from './style/global'
+import { ResetStyle } from './style/reset'
+
 function App () {
   return (
     <div className="App">
+      <ResetStyle/>
+      <GlobalStyle/>
     </div>
   )
 }
